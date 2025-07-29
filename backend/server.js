da46d5964883f894eb07f576cd1261121bb2f205
Mehul Joshi <87930901+MehulJoshi2002@users.noreply.github.com>
2025-07-29 15:28:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://blogzzzz.netlify.app'],
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
